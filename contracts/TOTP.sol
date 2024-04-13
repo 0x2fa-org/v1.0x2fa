fc0db5fabafc4212ac23c36f6e498266ddb5e9cc
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2024-04-13 17:13:34 +0200

    chore: docs for the interface and contract

--- contracts/TOTP.sol
+++ contracts/TOTP.sol
@@ -6,18 +6,39 @@ pragma solidity 0.8.24;
 
 import {Sapphire} from "@oasisprotocol/sapphire-contracts/contracts/Sapphire.sol";
 
+/**
+ * @title TOTP
+ * @dev Implements Time-Based One-Time Password (TOTP) functionality.
+ */
 contract TOTP {
+    /// @notice The seed used for generating TOTP.
     bytes32 private seed;
+    
+    /// @notice The time step in seconds for the TOTP algorithm.
     uint256 constant Timestep = 30;
+
+    /// @notice The number of digits in the TOTP.
     uint256 constant Digits = 6;
+
+    /// @notice The modulus used in the TOTP algorithm.
     uint256 constant Modulus = 10 ** Digits;
 
+    /// @notice Mapping of user addresses to their domain strings.
     mapping (address => string[]) public userDomains;
 
+    /**
+     * @dev Constructor that generates a random seed.
+     */
     constructor() {
         seed = bytes32(Sapphire.randomBytes(32, ""));
     }
 
+    /**
+     * @dev Generates a TOTP for a given domain and sender.
+     * @param _domain The domain for which to generate the TOTP.
+     * @param _sender The sender for which to generate the TOTP.
+     * @return The generated TOTP.
+     */
     function generate(
         string memory _domain,
         address _sender
@@ -28,14 +49,20 @@ contract TOTP {
         return uint256(keccak256(hmac)) % Modulus;
     }
 
-    function createDomainGroup(string memory _domain) external {
-        // Implement your logic here
-    }
-
+    /**
+     * @dev Adds a sender to a domain group.
+     * @param _domain The domain group to join.
+     * @param _sender The sender to add to the domain group.
+     */
     function joinDomainGroup(string memory _domain, address _sender) external {
         userDomains[_sender].push(_domain);
     }
 
+    /**
+     * @dev Removes a sender from a domain group.
+     * @param _domain The domain group to leave.
+     * @param _sender The sender to remove from the domain group.
+     */
     function leaveDomainGroup(string memory _domain, address _sender) external {
         string[] storage domains = userDomains[_sender];
         for (uint256 i = 0; i < domains.length; i++) {
@@ -47,6 +74,13 @@ contract TOTP {
         }
     }
 
+    /**
+     * @dev Verifies a TOTP for a given domain and sender.
+     * @param _domain The domain for which to verify the TOTP.
+     * @param _sender The sender for which to verify the TOTP.
+     * @param _code The TOTP to verify.
+     * @return _isValid Whether the TOTP is valid.
+     */
     function verify(
         string memory _domain,
         address _sender,
@@ -55,10 +89,20 @@ contract TOTP {
         return generate(_domain, _sender) == _code;
     }
 
+    /**
+     * @dev Gets the current time step.
+     * @return _timestep The current time step.
+     */
     function getCurrentTimeStep() public view returns (uint256 _timestep) {
         return block.timestamp / Timestep;
     }
 
+    /**
+     * @dev Derives a secret for a given domain and sender.
+     * @param _domain The domain for which to derive the secret.
+     * @param _sender The sender for which to derive the secret.
+     * @return _secret The derived secret.
+     */
     function deriveSecret(
         string memory _domain,
         address _sender
@@ -66,13 +110,24 @@ contract TOTP {
         return bytes32(keccak256(abi.encodePacked(seed, _domain, _sender)));
     }
 
+    /**
+     * @dev Calculates the HMAC for a given secret and data.
+     * @param _secret The secret for which to calculate the HMAC.
+     * @param _data The data for which to calculate the HMAC.
+     * @return _hmac The calculated HMAC.
+     */
     function calculateHMAC(
         bytes32 _secret,
         uint256 _data
-    ) public pure returns (bytes memory _hmac) {
+    ) internal pure returns (bytes memory _hmac) {
         return abi.encodePacked(keccak256(abi.encodePacked(_secret, _data)));
     }
 
+    /**
+     * @dev Gets the domains for a given sender.
+     * @param _sender The sender for which to get the domains.
+     * @return The domains for the sender.
+     */
     function getDomains(address _sender) public view returns (string[] memory) {
         return userDomains[_sender];
     }
--- contracts/interface/ITOTP.sol
+++ contracts/interface/ITOTP.sol
@@ -3,11 +3,47 @@
 pragma solidity ^0.8.24;
 
 interface ITOTP {
-  function generate(string memory _domain, address _sender) external;
-  function createDomainGroup(string memory _domain) external;
-  function joinDomainGroup(address _sender) external;
-  function verify(string memory _domain, address _sender, uint256 _code) external;
+  /**
+   * @dev Generates a OTP for a given domain and sender.
+   * @param _domain The domain for which the OTP is to be generated.
+   * @param _sender The address of the sender.
+   * @return Returns the generated OTP.
+   */
+  function generate(string memory _domain, address _sender) external view returns (uint256);
+
+  /**
+   * @dev Adds a sender to a domain group.
+   * @param _domain The domain group to join.
+   * @param _sender The address of the sender.
+   */
+  function joinDomainGroup(string memory _domain, address _sender) external;
+
+  /**
+   * @dev Removes a sender from a domain group.
+   * @param _domain The domain group to leave.
+   * @param _sender The address of the sender.
+   */
+  function leaveDomainGroup(string memory _domain, address _sender) external;
+
+  /**
+   * @dev Verifies a OTP for a given domain and sender.
+   * @param _domain The domain for which the OTP is to be verified.
+   * @param _sender The address of the sender.
+   * @param _code The OTP to verify.
+   * @return Returns true if the OTP is valid, false otherwise.
+   */
+  function verify(string memory _domain, address _sender, uint256 _code) external view returns (bool);
+
+  /**
+   * @dev Gets the current time step.
+   * @return Returns the current time step.
+   */
   function getCurrentTimeStep() external view returns (uint256);
-  function deriveSecret(string memory _domain, address _sender) external view returns (bytes32);
-  function calculateHMAC(bytes32 _secretKey, uint256 _data) external view returns (bytes32);
+
+  /**
+   * @dev Gets the domains for a given sender.
+   * @param _sender The address of the sender.
+   * @return Returns a list of domains for the sender.
+   */
+  function getDomains(address _sender) external view returns (string[] memory);
 }

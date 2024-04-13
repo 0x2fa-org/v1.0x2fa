cadc2f809b9676810b2a326082cc4daff25fa03c
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2024-04-13 21:40:48 +0200

    fix: lagging transition on mobile (low fps)

--- app/src/components/ui/progress.tsx
+++ app/src/components/ui/progress.tsx
@@ -1,14 +1,14 @@
 "use client"
 
-import * as React from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+>(({ className, value, color, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -18,7 +18,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-gray-900 transition-all dark:bg-gray-50"
+      className={cn("h-full w-full flex-1 dark:bg-gray-50", color)}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
